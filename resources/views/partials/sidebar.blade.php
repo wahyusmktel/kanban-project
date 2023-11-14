3c82c91c55b5c6bc528efbe0b31dae653cb5456d
Wahyu Rahmat Hidayat <wahyu@smktelkom-lpg.sch.id>
2023-11-14 21:51:21 +0700

    Apply commoon layout

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,12 @@
+<div class="sidebar">
+    <div class="sidebar-container">
+      <a class="sidebar-link" href="{{ route('home') }}">
+        <span class="material-icons sidebar-icon">home</span>
+        <p class="sidebar-text">Home</p>
+      </a>
+      <a class="sidebar-link" href="{{ route('tasks.index') }}">
+        <span class="material-icons sidebar-icon">list</span>
+        <p class="sidebar-text">Task List</p>
+      </a>
+    </div>
+  </div>
